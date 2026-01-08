4c7e6cbc3677d7de099c673d910557d40340c21d
thesauteedlemon <137444116+thesauteedlemon@users.noreply.github.com>
2026-01-07 23:05:15 -0600

    Create spending.ts

---
+++ data/spending.ts
@@ -0,0 +1,16 @@
+import { SpendingData } from "@/lib/types";
+
+export const spending: SpendingData[] = [
+  {
+    year: 1965,
+    totalUSD: 10000000000,
+    medicareUSD: 0,
+    medicaidUSD: 0,
+  },
+  {
+    year: 2020,
+    totalUSD: 4200000000000,
+    medicareUSD: 830000000000,
+    medicaidUSD: 670000000000,
+  },
+];
