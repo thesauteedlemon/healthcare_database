2d3230e5bae35da1d4946e19fcc7ccec2e8b6b57
thesauteedlemon <137444116+thesauteedlemon@users.noreply.github.com>
2026-01-07 23:05:50 -0600

    Create lifeExpectancy.ts

---
+++ data/lifeExpectancy.ts
@@ -0,0 +1,7 @@
+import { LifeExpectancyData } from "@/lib/types";
+
+export const lifeExpectancy: LifeExpectancyData[] = [
+  { year: 1960, expectancy: 69.7 },
+  { year: 1980, expectancy: 73.7 },
+  { year: 2020, expectancy: 78.9 },
+];
