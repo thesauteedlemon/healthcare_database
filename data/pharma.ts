46d3f5e974320fac35b184e8cad77f73704e60a9
thesauteedlemon <137444116+thesauteedlemon@users.noreply.github.com>
2026-01-07 23:11:58 -0600

    Create pharma.ts

---
+++ data/pharma.ts
@@ -0,0 +1,22 @@
+import { PharmaEvent } from "@/lib/types";
+
+export const pharma: PharmaEvent[] = [
+  {
+    year: 1987,
+    product: "Prozac",
+    company: "Eli Lilly",
+    indication: "Depression",
+  },
+  {
+    year: 2013,
+    product: "Sovaldi",
+    company: "Gilead",
+    indication: "Hepatitis C",
+  },
+  {
+    year: 2021,
+    product: "Comirnaty",
+    company: "Pfizer/BioNTech",
+    indication: "COVID-19",
+  },
+];
